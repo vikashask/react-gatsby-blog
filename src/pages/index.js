defd8539de0c921fd7fe7a26a9a5ccd903c39350
vikash <vikashraj144@gmail.com>
2020-04-11 00:13:45 +0530

    index page created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,99 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
+import { Link, graphql } from "gatsby"
 import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    {/* <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> */}
-  </Layout>
-)
+const IndexPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges.filter(
+    edge => edge.node.frontmatter.template === "post"
+  )
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article
+            key={node.fields.slug}
+            style={{
+              marginTop: `-15px`,
+              marginBottom: `70px`,
+            }}
+          >
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 10),
+                }}
+              >
+                <Link
+                  style={{
+                    boxShadow: `none`,
+                    color: "#000000",
+                    fontSize: `21px`,
+                    fontWeight: `400`,
+                    fontFamily: `Montserrat`,
+                  }}
+                  to={node.fields.slug}
+                >
+                  {title}
+                </Link>
+              </h3>
+              <small
+                style={{
+                  color: "#B7B7B7",
+                  fontSize: `11px`,
+                  fontWeight: `300`,
+                }}
+              >
+                {node.frontmatter.date}
+              </small>
+            </header>
+            <section
+              style={{ color: "#888888", fontSize: `17px`, fontWeight: `300` }}
+            >
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            template
+            title
+            tags
+            description
+            slug
+            category
+          }
+        }
+      }
+    }
+  }
+`
