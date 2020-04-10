9944f0f22b57b1d58808cd6d092d7caecafbfa23
vikash <vikashraj144@gmail.com>
2020-04-11 00:13:04 +0530

    page created for work and blog gatsby-transformer-remark plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,28 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-   
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
