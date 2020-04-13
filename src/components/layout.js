f9cbcc10a414402a9ec73e48075d43e331149f90
vikash <vikashraj144@gmail.com>
2020-04-13 13:42:04 +0530

    layout added

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,7 +12,9 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./Header/index"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -24,8 +26,11 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+    <div>
+      <header>
+        {" "}
+        <Header location={location} />{" "}
+      </header>
       <div
         style={{
           margin: `0 auto`,
@@ -33,14 +38,26 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
+        <main
+        style={{
+          minHeight: `calc(100vh - 120px)`,
+          border: `0.5px solid #EEEEEE`,
+          borderBottom: `0px`,
+          borderRadius: `10px 10px 0px 0px`,
+          paddingLeft: `30px`,
+          paddingRight: `30px`,
+          paddingTop: `-20px`,
+        }}
+      >
+        {children}
+      </main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
+    </div>
   )
 }
 
